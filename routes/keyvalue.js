2917160fa732d089d4b1099816a44c3462811035
Martin Scharm <martin@binfalse.de>
2025-07-07 13:54:13 +0200

    updgrading the implementation to use an sqlite db for storage
    that way we can
    * easily check what's stored
    * modify the stored values
    * keep the storage even after restart

--- routes/keyvalue.js
+++ routes/keyvalue.js
@@ -1,13 +1,21 @@
 var express = require('express');
 var router = express.Router();
+const {Database} = require('sqlite3').verbose();
 
-let keyValueStore = []
+const database = new Database('pace-cache.db');
+database.run(`
+    CREATE TABLE IF NOT EXISTS kv
+    (
+        key TEXT PRIMARY KEY,
+        gid TEXT,
+        value TEXT
+    ) STRICT
+`);
 
-const responseBody = (responseValues) => ({ d: { results: responseValues}})
-
+const responseBody = (responseValues) => ({d: {results: responseValues}})
 
 /* GET full key value store */
-router.get('/', function(req, res, next) {
+router.get('/mock-kv-store', function (req, res, next) {
     console.log(req.query);
     if (Object.keys(req.query).length > 0) {
         const reqKeyString = req.query['$filter']
@@ -20,28 +28,51 @@ router.get('/', function(req, res, next) {
             return
         }
         const indexEnd = reqKeyString.lastIndexOf(qTerminator);
-        const filter = reqKeyString.substring(indexStart, indexEnd);
-        let output = [ ...keyValueStore.filter((entry) => entry.key === filter)];
-        console.log("Found key: ", filter, responseBody(output));
-        res.send(responseBody(output));
+        const key = reqKeyString.substring(indexStart, indexEnd);
+        const query = database.prepare('SELECT * FROM kv WHERE key = ?');
+        query.get(key, (err, row) => {
+
+            if (err) res.status(500).send(new Error(err));
+
+            if (row) {
+                console.log('row', row)
+                res.send(responseBody([{
+                    key: row.key,
+                    gid: row.gid,
+                    value: Buffer.from(row.value).toString('base64')
+                }]));
+            } else
+                res.send(responseBody([]));
+        });
+        query.finalize()
     } else {
-        console.log("Full Store Content: ", responseBody(keyValueStore));
-        res.send(responseBody(keyValueStore));
+        database.all('SELECT * FROM kv OrDER BY key', (err, rows) => {
+            if (err) res.status(500).send(new Error(err));
+            else if (rows) {
+                const allEntries = rows.map((row) => (
+                    {
+                        key: row.key,
+                        gid: row.gid,
+                        value: Buffer.from(row.value).toString('base64')
+                    }
+                ))
+                res.send(responseBody(allEntries));
+            } else {
+                res.send(responseBody([]));
+            }
+            res.end()
+
+        });
     }
 });
 
 /* POST new values to key-value-store */
-router.post('/', function(req, res, next) {
-    console.log("Posting: ", req.body);
-    const entryIndex = keyValueStore.findIndex((item) => item.key === req.body.key);
-    if ( entryIndex > -1 ) {
-        keyValueStore[entryIndex] = { ...req.body, timestamp: new Date().toISOString() };
-    } else {
-        keyValueStore.push({ ...req.body, timestamp: new Date().toISOString() });
-    }
-    keyValueStore = [...keyValueStore];
-
-    res.send()
+router.post('/mock-kv-store', function (request, res, next) {
+    console.log("Posting: ", request.body.key);
+    const insert = database.prepare('INSERT OR REPLACE INTO kv (key, gid, value) VALUES (?, ?, ?)');
+    insert.run(request.body.key, request.body.gid, Buffer.from(request.body.value, 'base64').toString('utf8'));
+    insert.finalize()
+    res.end();
 })
 
 module.exports = router;
